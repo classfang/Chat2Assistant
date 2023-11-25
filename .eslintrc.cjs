1d19772d8866e6b890aa34c42fd553a577573c8e
Junki <fangjunjievip@hotmail.com>
2023-11-25 17:27:39 +0800

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     'vue/require-default-prop': 'off',
     'vue/multi-word-component-names': 'off',
-    'vue/no-v-html': 'off'
+    'vue/no-v-html': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 }
