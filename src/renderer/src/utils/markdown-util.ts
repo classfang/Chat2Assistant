58a8dacd869257d05f02b4a69eec83e0e00e8e77
Junki <fangjunjievip@hotmail.com>
2023-12-01 16:12:24 +0800

    fix: some optimization

--- src/renderer/src/utils/markdown-util.ts
+++ src/renderer/src/utils/markdown-util.ts
@@ -1,11 +1,36 @@
 import MarkdownIt from 'markdown-it'
-import MarkdownItHighlight from 'markdown-it-highlightjs'
+import hljs from 'highlight.js'
+import ClipboardJS from 'clipboard'
 import 'highlight.js/scss/github-dark.scss'
+import '@renderer/assets/css/markdown-code.less'
+import { Message } from '@arco-design/web-vue'
+import i18n from '@renderer/i18n'
 
-const markdown = new MarkdownIt().use(MarkdownItHighlight)
+const { t } = i18n.global
+
+const clipboard = new ClipboardJS('.code-header-copy')
+clipboard.on('success', () => {
+  Message.success(t('common.copySuccess'))
+})
+
+const markdown = new MarkdownIt({
+  highlight: (str: string, lang: string) => {
+    let codeHtml = `<code class="hljs language-${lang}">${hljs.highlightAuto(str).value}</code>`
+    codeHtml =
+      `<div class="code-header">
+        <div>${lang}</div>
+        <div class="code-header-copy" data-clipboard-text="${str}">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon-sm"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C10.8954 4 10 4.89543 10 6H14C14 4.89543 13.1046 4 12 4ZM8.53513 4C9.22675 2.8044 10.5194 2 12 2C13.4806 2 14.7733 2.8044 15.4649 4H17C18.6569 4 20 5.34315 20 7V19C20 20.6569 18.6569 22 17 22H7C5.34315 22 4 20.6569 4 19V7C4 5.34315 5.34315 4 7 4H8.53513ZM8 6H7C6.44772 6 6 6.44772 6 7V19C6 19.5523 6.44772 20 7 20H17C17.5523 20 18 19.5523 18 19V7C18 6.44772 17.5523 6 17 6H16C16 7.10457 15.1046 8 14 8H10C8.89543 8 8 7.10457 8 6Z" fill="currentColor"></path></svg>
+          <span>Copy</span>
+        </div>
+      </div>` + codeHtml
+    return `<pre>${codeHtml}</pre>`
+  }
+})
 
 export const renderMarkdown = (content: string, isLoading: boolean) => {
   let htmlCode = markdown.render(content)
+
   // 加载中，显示闪烁光标
   if (isLoading) {
     // 找到所有闭合标签
@@ -26,5 +51,6 @@ export const renderMarkdown = (content: string, isLoading: boolean) => {
         htmlCode.substring(lastCloseTagIndex)
     }
   }
+
   return htmlCode
 }
